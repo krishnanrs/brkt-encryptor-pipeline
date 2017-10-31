13cacd478cc9bf5850a229423b59ba7c42045957
Krishnan Ramanathan <krishnan@brkt.com>
2017-10-31 13:21:01 -0700

    Update task.sh to support multi-region encryption
    Updates to task.sh to encrypt the stemcell AMI in all supported Bracket regions

--- pcf-pipelines/brkt/tasks/create-brkt-stemcell/task.sh
+++ pcf-pipelines/brkt/tasks/create-brkt-stemcell/task.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 ROOT="$PWD"
 FILENAME="$(basename ${ROOT}/base-stemcell/*.tgz)"
@@ -9,28 +9,40 @@ pip install brkt-cli
 tmp_dir="/tmp/$$"
 mkdir -p $tmp_dir
 
+# Update this line for any additional supported Bracket regions
+BRKT_REGIONS="us-east-1 us-east-2 us-west-1 us-west-2 eu-central-1 eu-west-1"
+
 # Change this to refer to the actual location of the base stemcell
 tar zxf ${ROOT}/base-stemcell/light-bosh-stemcell-*-aws-xen-hvm-ubuntu-trusty-go_agent.tgz -C $tmp_dir
-ami_id=`cat ${tmp_dir}/stemcell.MF | grep $AWS_REGION | awk '{print $2}'`
 
 # auth_cmd="brkt auth --email $EMAIL --password $PASSWORD --root-url $URL"
 # export BRKT_API_TOKEN=`$auth_cmd`
 
 echo "${CA_CERT}" > ${ROOT}/ca.crt
 
-cmd="brkt aws encrypt --service-domain $SERVICE_DOMAIN --region $AWS_REGION --metavisor-version ${METAVISOR_VERSION} --ca-cert ${ROOT}/ca.crt --token ${BRKT_TOKEN} $ami_id"
-`$cmd > ${tmp_dir}/encrypt.log 2>&1`
-if [ `echo $?` -ne 0 ]; then
-    echo "Bracket encryption failed. Please check encryption logs at ${tmp_dir}/encrypt.log"
-else
+for region in $BRKT_REGIONS; do
+	ami_id=`cat ${tmp_dir}/stemcell.MF | grep region | awk '{print $2}'`
+	echo "Encrypting stemcell image $ami_id in $region"
+	# cmd="brkt aws encrypt --service-domain $SERVICE_DOMAIN --region $region --metavisor-version ${METAVISOR_VERSION} --ca-cert ${ROOT}/ca.crt --token ${BRKT_TOKEN} $ami_id"
+    cmd="brkt aws encrypt --service-domain $SERVICE_DOMAIN --region $region --metavisor-version ${METAVISOR_VERSION} --token ${BRKT_TOKEN} $ami_id"
+    $cmd | tee ${tmp_dir}/encrypt_$region.log &
+done
+
+# Wait for all background tasks to complete
+wait
+
+for region in $BRKT_REGIONS; do
     # Get output AMI from the encrypt command
-    output_ami=`tail -1 ${tmp_dir}/encrypt.log | awk '{print $1}'`
-    rm -f $tmp_dir/encrypt.log
-    sed -i "s/$ami_id/$output_ami/" ${tmp_dir}/stemcell.MF
-    pushd $tmp_dir > /dev/null
-    # Change this to preserve the stemcell name/version etc.
-    tar czf "${ROOT}/brkt-stemcell/${FILENAME}" `ls`
-    popd > /dev/null
-fi
+    output_ami=`tail -1 ${tmp_dir}/_$region.log | awk '{print $1}'`
+    if [[ $output_ami =~ ^ami- ]]; then
+    	sed -i "s/\b$region\b.*$/$region: $output_ami/" ${tmp_dir}/stemcell.MF
+    else
+    	echo "Encryption failed in region $region"
+    fi
+done
+
+pushd $tmp_dir > /dev/null
+tar czf "${ROOT}/brkt-stemcell/${FILENAME}" `ls`
+popd > /dev/null
 
 rm -rf $tmp_dir
